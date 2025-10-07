a7b5cb9c77be33423e950361507858c061e5eb1f
HungLeCasp <hung.le@caspeco.com>
2025-10-07 21:27:20 +0700

    add src
    add src

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import SearchBox from "./SearchBox";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-start">
+      <SearchBox />
+    </div>
+  );
+}
